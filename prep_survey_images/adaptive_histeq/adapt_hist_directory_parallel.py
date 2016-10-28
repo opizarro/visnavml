0bb19b99144204113a9786fde3171770c4855ddf
Oscar <opizarro73@gmail.com>
2016-10-28 14:41:24 +1100

    parallel version

---
+++ prep_survey_images/adaptive_histeq/adapt_hist_directory_parallel.py
@@ -0,0 +1,75 @@
+# adaptive histogram equalisation (CLAHE) for all images in a folder
+# inputs: 
+#   folder with images
+#   name of output folder
+# output: folder with 'corrected images'
+
+import numpy as np
+import cv2
+import glob
+import os
+from joblib import Parallel, delayed
+import multiprocessing
+
+inputdir = '/media/water/2016/PROCESSED_DATA/Jamaica201607/r20160709_151200_pr16_region3_dense_ns/i20160709_151200_cv'
+inputext = 'png'
+outputdir = '/media/water/2016/PROCESSED_DATA/Jamaica201607/r20160709_151200_pr16_region3_dense_ns/i20160709_151200_clahe2'
+
+def maybe_makedir(dirname, force=False):
+  if os.path.isdir(dirname) and not force:
+    # You may override by setting force=True.
+    print('%s already present - Skipping making dir' % (dirname))
+  else:
+    print('Making dir %s.' % dirname)
+    os.makedirs(dirname)
+  return
+
+maybe_makedir(outputdir)
+
+# for each image in the input folder
+# read image, run CLAHE and then save with same name in output folder
+
+inputpath = os.path.join(inputdir,'*.' + inputext)
+print inputdir
+print inputpath
+
+clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
+
+
+def colorCLAHE(imagename,outputdir,clahe_arg):
+    img = cv2.imread(imagename,1) # assumes colour image
+    cv2.imshow("img",img)
+
+    # convert image to LAB
+    lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    cv2.imshow("lab",lab)
+
+    # split into channels
+    l,a,b = cv2.split(lab)
+    cv2.imshow('l_channel', l)
+    cv2.imshow('a_channel', a)
+    cv2.imshow('b_channel', b)
+
+    # apply CLAHE to L channel
+
+    cl = clahe.apply(l)
+    cv2.imshow('CLAHE output', cl)
+
+    # merge channels
+    limg = cv2.merge((cl,a,b))
+    cv2.imshow('limg', limg)
+
+    #convert back to BGR
+    final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+    cv2.imshow('final', final)
+
+    outfile = os.path.join(outputdir, os.path.basename(imagename))
+    cv2.imwrite(outfile, final)
+
+num_cores = multiprocessing.cpu_count()
+
+Parallel(n_jobs=num_cores)(delayed(colorCLAHE)(file,outputdir,clahe) for file in glob.glob(inputpath))
+
+
+
+
